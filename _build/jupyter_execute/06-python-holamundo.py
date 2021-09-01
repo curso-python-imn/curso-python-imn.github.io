959f02092cbb5c5c7b3181aab140e21e20d58ca6
Manuel Vargas <mfvargas@gmail.com>
2021-08-31 23:27:43 -0600

    Modificar capítulo sobre Python - holamundo

--- _build/jupyter_execute/06-python-holamundo.py
+++ _build/jupyter_execute/06-python-holamundo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # El programa "Hola Mundo"
-# El programa **"Hola Mundo"** es frecuentemente el primer programa que se escribe para aprender un nuevo lenguaje de programación y probar su sintaxis básica. El programa simplemente imprime en la pantalla la hilera de texto "Hola Mundo" o una similar.
+# # Python - el programa "Hola Mundo"
+# El programa **"Hola Mundo"** es frecuentemente el primer programa que se escribe para aprender un nuevo lenguaje de programación y probar su sintaxis básica. Este programa simplemente imprime en la pantalla la hilera de texto "Hola Mundo" o una similar.
 # 
 # La tradición del uso de este programa en el aprendizaje de la programación de computadoras tiene su origen en el libro [_The C Programming Language_](https://en.wikipedia.org/wiki/The_C_Programming_Language), escrito en 1978 por Bryan W. Kernighan y Dennis M. Ritchie. El programa de ejemplo de este libro imprime la hilera en inglés **"hello, world"** (en minúscula, con una coma entre las palabras y sin signos de admiración). Aunque este el texto que le dio popularidad, el programa había sido utilizado previamente en el [tutorial del lenguaje de programación B](https://www.bell-labs.com/usr/dmr/www/bintro.html), de Bryan W. Kernighan.
 # 
